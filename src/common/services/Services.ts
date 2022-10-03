7bc132a5b46115acb2eee3235297dcc291dc8f41
James <aslan.jnn@gmail.com>
2022-10-03 16:01:10 +1100

    Lay down the option to toggle mouse reporting

--- src/common/services/Services.ts
+++ src/common/services/Services.ts
@@ -210,6 +210,7 @@ export type LogLevel = 'debug' | 'info' | 'warn' | 'error' | 'off';
 export interface ITerminalOptions {
   allowProposedApi?: boolean;
   allowTransparency?: boolean;
+  allowMouseReporting?: boolean;
   altClickMovesCursor?: boolean;
   cols?: number;
   convertEol?: boolean;
